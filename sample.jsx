a5141fc7b278030bf3e1d9bf7dbc9ecc534218a1
Leo <43287692+Leo-fse@users.noreply.github.com>
2023-06-03 17:13:42 +0900

    Update sample.jsx

--- sample.jsx
+++ sample.jsx
@@ -95,52 +95,59 @@ export const EditableTable = () => {
 
   return (
     <>
-      <Table>
-        <thead>
-          <tr>
-            {Object.values(FIELD_NAMES).map((fieldName) => (
-              <th key={fieldName}>
-                {fieldName}
-                <TextInput
-                  onChange={(event) =>
-                    handleFilterChange(event.target.value, fieldName)
-                  }
-                />
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((row, index) => (
-            <tr key={index}>
-              {Object.values(FIELD_NAMES).map((fieldName) => (
-                <td key={fieldName}>
-                  {(fieldName === 'CRM_PLANT_ID' || fieldName === 'CRM_UNIT_ID') ? (
-                    <TextInput
-                      value={
-                        editedData[index]?.[fieldName] || row[fieldName] || ""
-                      }
-                      onChange={(event) =>
-                        handleChange(event.target.value, fieldName, index)
-                      }
-                    />
-                  ) : (
-                    <span>{row[fieldName] || ""}</span>
-                  )}
-                </td>
-              ))}
-              <td>
-                <Button
-                  onClick={() => handleUpdate(index)}
-                  disabled={!editedData[index] || Object.keys(editedData[index]).length === 0}
-                >
-                  Update
-                </Button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+    <Table>
+  <thead>
+    <tr>
+      {Object.values(FIELD_NAMES).map((fieldName) => (
+        <th 
+          key={fieldName} 
+          className="sticky top-0 bg-white z-10 border border-gray-300"
+        >
+          {fieldName}
+          <TextInput
+            onChange={(event) =>
+              handleFilterChange(event.target.value, fieldName)
+            }
+          />
+        </th>
+      ))}
+    </tr>
+  </thead>
+  <tbody>
+    {filteredData.map((row, index) => (
+      <tr key={index}>
+        {Object.values(FIELD_NAMES).map((fieldName) => (
+          <td 
+            key={fieldName} 
+            className="border border-gray-300"
+          >
+            {(fieldName === 'CRM_PLANT_ID' || fieldName === 'CRM_UNIT_ID') ? (
+              <TextInput
+                value={
+                  editedData[index]?.[fieldName] || row[fieldName] || ""
+                }
+                onChange={(event) =>
+                  handleChange(event.target.value, fieldName, index)
+                }
+              />
+            ) : (
+              <span>{row[fieldName] || ""}</span>
+            )}
+          </td>
+        ))}
+        <td>
+          <Button
+            onClick={() => handleUpdate(index)}
+            disabled={!editedData[index] || Object.keys(editedData[index]).length === 0}
+          >
+            Update
+          </Button>
+        </td>
+      </tr>
+    ))}
+  </tbody>
+</Table>
+
       <Modal opened={updateModalOpen} onClose={handleUpdateModalClose}>
         <Paper padding="md">
           <h1>Update Confirmation</h1>
