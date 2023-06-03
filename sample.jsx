9046a5761d08bf2252e94d31e6c1a48ef4d88e41
Leo <43287692+Leo-fse@users.noreply.github.com>
2023-06-03 16:19:13 +0900

    Update sample.jsx

--- sample.jsx
+++ sample.jsx
@@ -64,18 +64,33 @@ export const EditableTable = () => {
     setUpdateModalOpen(true);
   };
 
-  const handleConfirmedUpdate = async () => {
-    if (!updatedRow) {
-      console.error("Update failed: No row to update");
-      return;
-    }
-    const response = await fetch(process.env.NEXT_PUBLIC_API_UPDATE_ENDPOINT, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedRow),
-    });
+const handleConfirmedUpdate = async () => {
+  if (!updatedRow) {
+    console.error("Update failed: No row to update");
+    return;
+  }
+  const response = await fetch(process.env.NEXT_PUBLIC_API_UPDATE_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedRow),
+  });
+
+  if (response.ok) {
+    const updatedData = fetchedData.map((row, index) =>
+      index === updatedRow.index
+        ? { ...row, ...editedData[updatedRow.index] }
+        : row
+    );
+    setData(updatedData);
+  } else {
+    console.error("Update failed: " + response.statusText);
+  }
+
+  handleUpdateModalClose(); // モーダルを閉じる
+};
+
 
     if (response.ok) {
       const updatedData = fetchedData.map((row, index) =>
